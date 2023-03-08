ddfe10b955236d152d4df7aace48755885b5a3e6
Philipp Staehr <philippstaehr@gmail.com>
2023-03-08 20:39:05 +0100

    route new added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,11 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
 end
